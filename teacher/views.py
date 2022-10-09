03be8258e5f035ecfbe3d22d160d0124dc3e4e4c
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-10-09 20:00:33 -0300

    feat: Cria a view de professor

--- teacher/views.py
+++ teacher/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.views import Response
+from teacher.models import Professor
+from teacher.serializers import ProfessorSerializer
+from rest_framework.status import HTTP_200_OK
 
-# Create your views here.
+class ProfessorAPIView(APIView):
+    def get(self, request, format=None):
+        professores = Professor.objects.all()
+        serializer = ProfessorSerializer(professores, many=True)
+        return Response(serializer.data, status=HTTP_200_OK)

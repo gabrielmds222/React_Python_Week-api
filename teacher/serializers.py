5fe58d5edc1ab3ba67775e3000c312ef68f211bd
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-10-09 22:36:24 -0300

    feat: Validação customizada do nome do usuário

--- teacher/serializers.py
+++ teacher/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from teacher.models import Professor, Aula
+from django.forms import ValidationError
 
 class ProfessorSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,6 +10,11 @@ class ProfessorSerializer(serializers.ModelSerializer):
 class CadastrarAulaSerializer(serializers.Serializer):
     email = serializers.EmailField(max_length=255)
     nome = serializers.CharField(max_length=100)
+
+    def validate_nome(self, value):
+        if len(value) < 3:
+            raise ValidationError("O nome deve conter pelo menos 3 caracteres")
+        return value    
 
 class AulaSerializer(serializers.ModelSerializer):
     class Meta:

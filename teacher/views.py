31cc5a559faa522f8ec5f765c557cf19f3f70470
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-10-09 22:26:34 -0300

    feat: Cria view das aulas

--- teacher/views.py
+++ teacher/views.py
@@ -1,11 +1,27 @@
 from rest_framework.views import APIView
 from rest_framework.views import Response
-from teacher.models import Professor
-from teacher.serializers import ProfessorSerializer
-from rest_framework.status import HTTP_200_OK
+from teacher.models import Professor, Aula
+from teacher.serializers import ProfessorSerializer, CadastrarAulaSerializer, AulaSerializer
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from django.shortcuts import get_object_or_404
 
 class ProfessorApiView(APIView):
     def get(self, request, format=None):
         professores = Professor.objects.all()
         serializer = ProfessorSerializer(professores, many=True)
         return Response(serializer.data, status=HTTP_200_OK)
+
+class CadastrarAulaApiView(APIView):
+    def post(self, request, id, format=None):
+        professor = get_object_or_404(Professor, id=id)
+        serializer = CadastrarAulaSerializer(data=request.data)
+        if serializer.is_valid():
+            aula = Aula(
+                nome=serializer.validated_data.get('nome'),
+                email=serializer.validated_data.get('email'),
+                professor=professor
+            )
+            aula.save()
+            aula_serializer = AulaSerializer(aula, many=False)
+            return Response(aula_serializer.data, status=HTTP_201_CREATED)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
--- treinawebApi/urls.py
+++ treinawebApi/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app.views import HomeApiView
-from teacher.views import ProfessorApiView
+from teacher.views import ProfessorApiView, CadastrarAulaApiView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeApiView.as_view()),
     path('professores/', ProfessorApiView.as_view()),
+    path('professores/<int:id>/aulas', CadastrarAulaApiView.as_view()),
 ]

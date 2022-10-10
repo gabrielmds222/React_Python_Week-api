6b358b9fd279adc1bf3a1691b62ec84ea468fe63
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-10-09 22:16:55 -0300

    feat: Serilizer de cadastro

--- teacher/serializers.py
+++ teacher/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from teacher.models import Professor
+from teacher.models import Aula
 
 class ProfessorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Professor
         fields = '__all__'
+
+class CadastrarAulaSerializer(serializers.Serializer):
+    email = serializers.EmailField(max_length=255)
+    nome = serializers.CharField(max_length=100)

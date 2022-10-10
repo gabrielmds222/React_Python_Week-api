8891173c550bd4d9b53a2a629bdf74c341253c8f
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-10-09 22:41:49 -0300

    feat: Instala e configura o cors

--- treinawebApi/settings.py
+++ treinawebApi/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'app.apps.AppConfig',
     'teacher.apps.TeacherConfig',
     'rest_framework',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -124,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS: True
